9b8b2870f9ee6f03837143178248db0754053524
Nikola <88djony@gmail.com>
2021-08-10 14:33:06 -0700

    added client to app.js

--- book-search-engine/client/src/App.js
+++ book-search-engine/client/src/App.js
@@ -6,8 +6,23 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+const client = new AppoloClient({
+  request: (operation) => {
+    const token = localStorage.getItem("id_token");
+
+    operation.setContext({
+      headers: {
+        autorization: token ? `Bearer ${token}` : "",
+      },
+    });
+  },
+  url:"/graphql",
+})
+
 function App() {
   return (
+  <AppoloProvider client = {client}>
+
     <Router>
       <>
         <Navbar />
@@ -18,7 +33,11 @@ function App() {
         </Switch>
       </>
     </Router>
+    
+  </AppoloProvider>
+
   );
+
 }
 
 export default App;
